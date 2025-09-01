9b8c8fb5b529feba118b8c60e9ef65b644a2af15
Vítek <vitek@meloun.info>
2025-09-01 13:31:07 +0200

    Rename

--- spec/support/packeta_requests_helper.rb
+++ spec/support/packeta_requests_helper.rb
@@ -1,11 +1,11 @@
 module PacketaRequestsHelper
-  def api_response_mock(status: 'ok', &response)
+  def api_response_mock(status: 'ok', &response_creator)
     LibXML::XML::Document.new.tap do |result|
       response_node = LibXML::XML::Node.new('response')
       result_node = LibXML::XML::Node.new('result')
       status_node = LibXML::XML::Node.new('status')
 
-      response.call(result_node)
+      response_creator.call(result_node)
 
       status_node.content = status
       response_node << status_node
